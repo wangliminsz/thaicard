aacf94a3439ccb2f4e88306a12b87836d1f51672
wangliminsz <wang.limin.sz@gmail.com>
2023-12-30 18:54:50 +0800

    async

--- src/views/thaiCard.vue
+++ src/views/thaiCard.vue
@@ -184,7 +184,8 @@ export default {
 
                 // flex Json ----------------------------------------------
 
-                let the_text = this.contact.fields.name.replace(/\s+/g, '');
+                // let the_text = this.contact.fields.name.replace(/\s+/g, '');
+                let the_text = this.contact.fields.name..trimEnd();
                 let the_phone = this.contact.fields.phone
                 // let the_tel = this.contact.fields.tel
                 // let the_label = this.contact.fields.tel_label
